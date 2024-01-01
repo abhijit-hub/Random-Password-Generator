033db168bf48525e5e49fcd0e13ddc3369230110
Abhijit More <itsabhijitmore@gmail.com>
2024-01-02 00:49:02 +0530

    Ui and Js Changed

--- script.js
+++ script.js
@@ -1,55 +1,70 @@
- var alphabet=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','w','z'];
- var specailChar=['!','@','#','$'];
- var num=['1','2','3','4','5','6','7','8','9','0'];   // not need this array because random num can be use from randomNumArr
+var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'w', 'z'];
+var specailChar = ['!', '@', '#', '$'];
+var num = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];   // not need this array because random num can be use from randomNumArr
 
 
 
- // for loop for random num which run for user password len and push evry random num to radomNumArr
+// for loop for random num which run for user password len and push evry random num to radomNumArr
 
 
 
 
 
 
-function generatePassword(){
-  var randomCharArr=[];                      // array for random number for char index        
-  var randomNumArr=[];                       // array for random number for num index
-  for(var i=0;i<4;i++){                       // for loop for 4 char and 4 num
-    var randomNum=Math.floor(Math.random()*25);
+function generatePassword() {
+  var randomCharArr = [];                      // array for random number for char index        
+  var randomNumArr = [];                       // array for random number for num index
+  for (var i = 0; i < 7; i++) {                       // for loop for 4 char and 4 num
+    var randomNum = Math.floor(Math.random() * 25);
     randomCharArr.push(randomNum);
- 
-    randomNumArr.push(Math.floor(Math.random()*10))
- 
-   }
- 
-  var randomChar=Math.floor(Math.random()*4);            // for special char
-  var char=specailChar[randomChar];
- 
-  var firstChar=alphabet[randomCharArr[0]].toLocaleUpperCase();
- 
-  var final=firstChar+alphabet[randomCharArr[1]]+alphabet[randomCharArr[2]]+alphabet[randomCharArr[3]]
-     +char+randomNumArr[0]+randomNumArr[1]+randomNumArr[2]+randomNumArr[3];
 
-  document.getElementById("passwordinput").value=final;
+    randomNumArr.push(Math.floor(Math.random() * 10))
+
+  }
+
+  var randomChar = Math.floor(Math.random() * 4);            // for special char
+  var char = specailChar[randomChar];
+
+  var firstChar = alphabet[randomCharArr[0]].toLocaleUpperCase();
+
+  var final = firstChar;
+
+  for (var i = 1; i < randomCharArr.length; i++) {
+    final += alphabet[randomCharArr[i]];
+  }
+  final += char + randomNumArr[0] + randomNumArr[1] + randomNumArr[2] + randomNumArr[3];
+
+  document.getElementById("passwordinput").value = final;
 }
 
 
 
 
-$("button").on("click",function(){          //eventlistner for button click
-  
-  var html=this.innerHTML;
-  if(html=="Generate"){
-      generatePassword();
-      // $("input").css("borderColor","green");
-      $("input").addClass("second");
+$("button").on("click", function () {          //eventlistner for button click
 
-      
-      
-    }
+  var html = this.innerHTML;
+
+
+
+  if (html == "Generate") {
+    generatePassword();
+    // $("input").css("borderColor","green");
+    $("input").addClass("second");
+
+
+
+  }
+  document.getElementById("passwordinput").style.borderColor="#0aef69de";
+
 })
 
 function myFunction() {
+
+
+  if (document.getElementById("passwordinput").value == "") {
+    alert("Cilck On Generate!");
+    return;
+  }
   // Get the text field
   var copyText = document.getElementById("passwordinput");
 
@@ -57,7 +72,7 @@ function myFunction() {
   copyText.select();
   copyText.setSelectionRange(0, 99999); // For mobile devices
 
-   // Copy the text inside the text field
+  // Copy the text inside the text field
   navigator.clipboard.writeText(copyText.value);
 
   // Alert the copied text
